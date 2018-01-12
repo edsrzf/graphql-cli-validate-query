6b973a25a08542e010031db412dd1c65f2453e8a
Evan Shaw <evan@vendhq.com>
2018-01-12 22:08:00 +1300

    First commit

---
+++ validate-query.js
@@ -0,0 +1,43 @@
+exports.command = 'validate-query <file>'
+exports.desc = 'Validate GraphQL query'
+
+const fs = require('fs')
+const process = require('process')
+
+const chalk = require('chalk')
+const graphql = require('graphql')
+
+const loadSchema = (config) => {
+  try {
+    return graphql.buildSchema(config.getSchemaSDL())
+  } catch (error) {
+    for (loc of error.locations) {
+      console.log(chalk.red(`schema (${loc.line}, ${loc.column}): ${error.message}`))
+    }
+  }
+}
+
+const readFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => err ? reject(err) : resolve(data))
+  })
+}
+
+const validateQuery = async (schema, queryFile) => {
+  const queryText = await readFile(queryFile)
+  try {
+    const queryDoc = parse(queryText)
+    graphql.validate(schema, queryDoc)
+  } catch (error) {
+    for (loc of error.locations) {
+      console.log(chalk.red(`${queryFile} (${loc.line}, ${loc.column}): ${error.message}`))
+    }
+    process.exit(1)
+  }
+}
+
+exports.handler = async (context, argv) => {
+  const config = await context.getProjectConfig()
+  const schema = loadSchema(config)
+  await validateQuery(schema, argv.file)
+}
